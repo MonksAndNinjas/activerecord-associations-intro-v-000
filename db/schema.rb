80059f5ac2cca6b7e6483f10c16ffbe458302e4e
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-15 17:24:29 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,14 @@ ActiveRecord::Schema.define(version: 5) do
     t.string "name"
   end
 
+  create_table "genres", force: :cascade  do |t|
+    t.string "name"
+  end
+
   create_table "songs", force: :cascade do |t|
     t.string "name"
     t.integer "artist_id"
     t.integer "genre_id"
-  end
-
-  create_table "genres", force: :cascade  do |t|
-    t.string "name"
   end
 
 end
